d695cadf3c9b6c7fde68fac75c561b27248fc262
nejvej@gmail.com <nejvej@gmail.com>
2021-07-12 02:09:02 +0000

    external login i chart zavrseni

--- src/components/Authentication/SocialNetworks.js
+++ src/components/Authentication/SocialNetworks.js
@@ -1,67 +1,147 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import FacebookLogin from 'react-facebook-login';
-import { GoogleLogin } from 'react-google-login';
-
-// class App extends Component {
-
-//     constructor() {
-//         super();
-//         this.state = { isAuthenticated: false, user: null, token: ''};
-//     }
-
-//     logout = () => {
-//         this.setState({isAuthenticated: false, token: '', user: null})
-//     };
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import axios from 'axios'
+import { object } from 'yup';
+import { writeFile } from 'fs';
+ 
+const clientId = "647369311564-muhu77078797c8sn2pd4n5m591na28ks.apps.googleusercontent.com";
+ 
+function App() {
+ 
+  const [loading, setLoading] = useState('Loading...');
+  const [user, setUser] = useState(null);
+  
+  const handleLoginSuccess = (response) => {
+    //throw response;
+    const resp = axios.post("http://localhost:8000/api/Account/AddExternalLogin", response);
+    console.log("Login Success ", response);
+    setUser(response.profileObj);
+    setLoading();
+  }
+ 
+  const handleLoginFailure = error => {
+    console.log("Login Failure ", error);
+    setLoading();
+  }
+ 
+  const handleLogoutSuccess = (response) => {
+    console.log("Logout Success ", response);
+    setUser(null);
+  }
+ 
+  const handleLogoutFailure = error => {
+    console.log("Logout Failure ", error);
+  }
+ 
+  const handleRequest = () => {
+    setLoading("Loading...");
+  }
+ 
+  const handleAutoLoadFinished = () => {
+    setLoading();
+  }
+ 
+  return (
+    <div>
+      {user ? <div>
+        <GoogleLogout
+          clientId={clientId}
+          onLogoutSuccess={handleLogoutSuccess}
+          onFailure={handleLogoutFailure}
+        />
+        
+      </div> :
+        <GoogleLogin
+          clientId={clientId}
+          buttonText={loading}
+          onSuccess={handleLoginSuccess}
+          onFailure={handleLoginFailure}
+          onRequest={handleRequest}
+          onAutoLoadFinished={handleAutoLoadFinished}
+          isSignedIn={true}
+        />}
+    </div>
+  );
+}
+ 
+export default App;
+/*
+class App extends Component {
     
-//     facebookResponse = (e) => {};
+    
+    
+    constructor() {
+        super();
+        this.state = { isAuthenticated: false, user: null, token: ''};
+    }
 
-//     googleResponse = (e) => {};    
-//     onFailure = (error) => {
-//       alert(error);
-//     }    
+    logout = () => {
+        this.setState({isAuthenticated: false, token: '', user: null})
+    };
+    
+    facebookResponse = (e) => {};
 
-//     render() {
-//         let content = !!this.state.isAuthenticated ?
-//             (
-//                 <div>
-//                     <p>Authenticated</p>
-//                     <div>
-//                         {this.state.user.email}
-//                     </div>
-//                     <div>
-//                         <button onClick={this.logout} className="button">
-//                             Log out
-//                         </button>
-//                     </div>
-//                 </div>
-//             ) :
-//             (
-//                 <div>
-//                     <FacebookLogin
-//                         appId="502546801013232"
-//                         autoLoad={false}
-//                         fields="name,email,picture"
-//                         callback={this.facebookResponse} />
-//                     <GoogleLogin
-//                         clientId="647369311564-muhu77078797c8sn2pd4n5m591na28ks.apps.googleusercontent.com"
-//                         buttonText="Login"
-//                         onSuccess={this.googleResponse}//odi dedi sedi
-//                         onFailure={this.googleResponse}
-//                     />
-//                 </div>
-//             );
+    googleResponse = (e) => {};    
+    onFailure = (error) => {
+        const responseGoogle = response => {
+            console.log(response);
+        };
+    }    
+    onSuccess = () =>{
+        loading = useState('loading');
+        setLoading = useState('loading');
+        user = useState(null);
+        setUser = useState(null);
 
-//         return (
-//             <div className="App">
-//                 {content}
-//             </div>
-//         );
-//     }
-// }
+        console.log("Login Success ", response);
+        setUser(response.profileObj);
+        setLoading();
+    }
+    render() {
+        let content = !!this.state.isAuthenticated ?
+            (
+                <div>
+                    <p>Authenticated</p>
+                    <div>
+                        {this.state.user.email}
+                    </div>
+                    <div>
+                        <button onClick={this.logout} className="button">
+                            Log out
+                        </button>
+                    </div>
+                </div>
+            ) :
+            (
+                <div>
+                    <FacebookLogin
+                        appId="502546801013232"
+                        autoLoad={false}
+                        fields="name,email"
+                        callback={this.facebookResponse} />
+                    <GoogleLogin
+                        clientId="647369311564-muhu77078797c8sn2pd4n5m591na28ks.apps.googleusercontent.com"
+                        buttonText="Login"
+                        onSuccess={this.onFailure}
+                        onFailure={this.onSuccess}
+                        cookiePolicy={'single_host_origin'}
+                    />
+                </div>
+            );
+            
+        return (
+            <div className="App">
+                {content}
+            </div>
+        );
+    }
+}
 
-// export default App;
-
-
+export default App;
+*/
+//#region SN
+/*
 const SocialNetworks = () => {
     return (
         <div style={{ float: 'left', marginLeft: 0}}>
@@ -115,5 +195,5 @@ const SocialNetworks = () => {
     );
 }
 
-export default SocialNetworks
+export default SocialNetworks*/
 //#endregion
--- src/components/dashboard/DashboardHome.js
+++ src/components/dashboard/DashboardHome.js
@@ -3,16 +3,23 @@ import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { loggedUserSelector } from '../../store/selectors/AuthSelector';
 import DashBoardHomeItem from './DashboardHomeItem';
+import Chart from "react-google-charts";
 
 const DashBoardHome = () => {
 
     const user = useSelector(loggedUserSelector);
     const { push } = useHistory()
+    var React = require('react');
+    var Component = React.Component;
 
+    let incidentsNum = 5;
+    let workPlansNum = 10;
+    let safetyDocsNum = 11;
+    
     if (user === null || user === undefined) {
         push('/')
     }
-
+    
     return (<div style={{marginLeft: 150, height: 550}}>
         
         <table style={{marginLeft: 120, marginTop: 100, position: 'fixed'}}>
@@ -20,26 +27,26 @@ const DashBoardHome = () => {
             <tbody>
                 <tr>
                     <td>
-                        <DashBoardHomeItem name="My Incidents" num='0'/>
+                        <DashBoardHomeItem name="My Incidents" num={incidentsNum}/>
                     </td>
                     
                     <td>
                         <div style={{marginLeft: 50}}>
-                            <DashBoardHomeItem name="My Work plans" num='10'/>
+                            <DashBoardHomeItem name="My Work plans" num={workPlansNum}/>
                         </div>
                     </td>
 
                     <td>
                         <div style={{marginLeft: 50}}>
-                            <DashBoardHomeItem name="My Safety docs" num='11'/>
+                            <DashBoardHomeItem name="My Safety docs" num={safetyDocsNum}/>
                         </div>
                     </td>
 
                 </tr>
 
                 <tr>
-                    <td colSpan="2">
-                        <div className="ui raised container segment" style={{height: 190, width: 490, overflow: 'hidden', marginTop: 40}}>
+                    <td colSpan="1">
+                        <div className="ui raised container segment" style={{height: 190, width: 220, overflow: 'hidden', marginTop: 40}}>
                             <h3 style={{float: 'left', marginRight: 30}}> INCIDENTS </h3>
                             <button className="ui tiny secondary button" style={{marginLeft: 275}}> 
                                 <i className="cog icon" style={{paddingLeft : 5}}/>
@@ -49,13 +56,33 @@ const DashBoardHome = () => {
                     </td>
 
                     <td>
+                    <Chart
+                    width={'300'}
+                    height={'200'}
+                    chartType="PieChart"
+                    loader={<div>Loading Chart</div>}
+                    data={[
+                        ['Dokumenti', 'Broj'],
+                        ['Incidenti', incidentsNum],
+                        ['Planovi Rada', workPlansNum],
+                        ['Bezbedni Dokumenti', safetyDocsNum],
+                    ]}
+                    options={{
+                        title: 'My Daily Activities',
+                    }}
+                    rootProps={{ 'data-testid': '1' }}
+                    />
+                    </td>
+                    <td>
                         <div className="ui raised container segment" style={{height: 190, width: 220, overflow: 'hidden', marginTop: 40, marginLeft: 160}}>
                             <h3 style={{float: 'left', marginRight: 30}}> DOCUMENTS </h3>
                             <hr style={{marginTop: 30}}/>
                         </div>
+                        
                     </td>
+                
                 </tr>
-
+                
             </tbody>
         </table>
 
